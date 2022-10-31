810790c8e5087c7eec3e8e40f08a9d1c6c532fac
Yuhimchuk Vyacheslav <hhomebb@gmail.com>
2022-10-31 20:43:51 +0200

    7

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputRangeRef = document.querySelector("#font-size-control");
-
-const text = document.querySelector("#text");
-
-inputRangeRef.oninput = () => {
-  text.style.fontSize = inputRangeRef.value + "px";
-};
